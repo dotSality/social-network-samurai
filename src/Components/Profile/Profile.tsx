bf29a8b37fff63e660937551c95a343b3777f44c
dotSality <90727519+dotSality@users.noreply.github.com>
2021-09-29 20:20:37 +0300

    Lesson 13

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Profile = () => {
 return (<div className={'content'}>
-  (<img src='https://c4.wallpaperflare.com/wallpaper/784/819/740/ultra-wide-photography-beach-wallpaper-preview.jpg'/>
+  <img src='https://c4.wallpaperflare.com/wallpaper/784/819/740/ultra-wide-photography-beach-wallpaper-preview.jpg'/>
     <div>
         ava + description
     </div>
