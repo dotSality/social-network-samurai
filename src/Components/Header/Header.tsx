57d8f0724f4a1fea584d67dae6e436f8ebe35428
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-02 20:41:55 +0300

    Lesson 14 CSS modules
    added and learned CSS modules with a little bit of practice

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className={'header'}>
+    return <header className={s.header}>
         <img src='https://images.fastcompany.net/image/upload/w_1280,f_auto,q_auto,fl_lossy/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg'/>
     </header>
 }
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,22 +1,27 @@
 import React from "react";
+import s from './Profile.module.css'
 
 const Profile = () => {
-return (<div className={'content'}>
-  <img src='https://c4.wallpaperflare.com/wallpaper/784/819/740/ultra-wide-photography-beach-wallpaper-preview.jpg'/>
-    <div>
-        ava + description
-    </div>
-    My posts
-    <div>
-        New post
-    </div>
-    <div>
-        Post 1
-    </div>
-    <div>
-        Post 2
-    </div>
-</div>)
+    return (<div className={s.content}>
+        <img src='https://c4.wallpaperflare.com/wallpaper/784/819/740/ultra-wide-photography-beach-wallpaper-preview.jpg'/>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                    Post 1
+                </div>
+                <div className={s.item}>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    </div>)
 }
 
 export default Profile;
