44069f45411274ab5cc7df2e3f513e66d10b78d0
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-12 18:53:15 +0300

    lesson 18 completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from './Components/Dialogs/Dialogs';
 
 function App() {
     return (
         <div className={'app-wrapper'}>
             <Header/>
             <NavBar/>
-            <Profile/>
+            <div className={'app-wrapper-content'}>
+                <Dialogs/>
+            </div>
+            {/*<Profile/>*/}
         </div>
     );
 }
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css'
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return (<div className={s.content}>
+    return (<div>
         <img src='https://c4.wallpaperflare.com/wallpaper/784/819/740/ultra-wide-photography-beach-wallpaper-preview.jpg'/>
         <div>
             ava + description
